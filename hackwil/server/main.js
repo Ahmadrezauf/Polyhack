8bd262de972c4998e579e36956a0c17e9f23879d
jonathanlehner <lehnerj@student.ethz.ch>
2020-11-07 19:33:43 +0100

    basic setup working

---
+++ hackwil/server/main.js
@@ -0,0 +1,42 @@
+import { Meteor } from 'meteor/meteor';
+import { LinksCollection } from '/imports/api/links';
+import './methods.js';
+import './api.js'
+import './websockets.js'
+
+function insertLink({ title, url }) {
+  LinksCollection.insert({title, url, createdAt: new Date()});
+  
+}
+
+Meteor.startup(() => {
+  // If the Links collection is empty, add some data.
+  const path = require("path");
+  const project_dir = process.env.PWD;
+  const parent_dir = path.dirname(project_dir);
+  console.log(parent_dir);
+  // console.log(process.cwd()); // Meteor execution directory in .meteor/local
+  Meteor.call("spawnSensor", parent_dir+"/sensors/index.js");
+  
+  if (LinksCollection.find().count() === 0) {
+    insertLink({
+      title: 'Do the Tutorial',
+      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+    });
+
+    insertLink({
+      title: 'Follow the Guide',
+      url: 'http://guide.meteor.com'
+    });
+
+    insertLink({
+      title: 'Read the Docs',
+      url: 'https://docs.meteor.com'
+    });
+
+    insertLink({
+      title: 'Discussions',
+      url: 'https://forums.meteor.com'
+    });
+  }
+});
---
+++ hackwil/server/methods.js
@@ -0,0 +1,20 @@
+
+
+Meteor.methods({
+    'spawnSensor': (sensor_path) => {
+        const { exec } = require('child_process');
+        const ls = exec(`node ${sensor_path}`, function (error, stdout, stderr) {
+            if (error) {
+                console.log(error.stack);
+                console.log('Error code: '+error.code);
+                console.log('Signal received: '+error.signal);
+            }
+            console.log('Child Process STDOUT: '+stdout);
+            console.log('Child Process STDERR: '+stderr);
+        });
+
+        ls.on('exit', function (code) {
+        console.log('Child process exited with exit code '+code);
+        });
+    }
+});
