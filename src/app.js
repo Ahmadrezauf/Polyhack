b6a3e77688ca3761a1e50c08be12aedc7d137b96
agorji <alli.gorji@gmail.com>
2020-11-07 21:20:04 +0100

    Seperate rule base and evaluation

--- src/app.js
+++ src/app.js
@@ -5,21 +5,12 @@ let rawdata = fs.readFileSync('rules.json');
 let rules = JSON.parse(rawdata)['rules'];
 
 function then_func(facts, new_facts){
-    console.log("Ali");
-
     for(var key in new_facts){
         facts[key] = new_facts[key];
     }
 }
 
-async function run(rules) {
-    // facts
-    facts = {
-        Motion0: "motion",
-        Proximity0: 0.2
-    };
-    console.log(facts);
-
+async function create_rule_engine(rules, rools) {
     let rools_list = [];
     for(var rule_idx in rules) {
         const rule = rules[rule_idx];
@@ -29,15 +20,14 @@ async function run(rules) {
         // Add conditions
         for(var cond_idx in rule['conditions']){
             const condition = rule['conditions'][cond_idx];
-            // console.log(condition)
             operator = condition['operator'];
             if(operator === '='){
                 condition_list.push((facts) => facts[condition['sensor']] === condition['value']);
-                console.log(facts[condition['sensor']] === condition['value'])
+                // console.log(facts[condition['sensor']] === condition['value'])
             }
             else if(operator === '<'){
                 condition_list.push((facts) => facts[condition['sensor']] < condition['value']);
-                console.log(facts[condition['sensor']] < condition['value'])
+                // console.log(facts[condition['sensor']] < condition['value'])
             }
             else if(operator === '>'){
                 condition_list.push((facts) => facts[condition['sensor']] > condition['value']);
@@ -58,8 +48,6 @@ async function run(rules) {
             action = rule['actions'][action_idx];
             action_dict[action['actuator']] = action['value'];
         }
-        
-        await condition_list;
 
         // Add new rule
         const rools_rule = new Rule({
@@ -67,14 +55,39 @@ async function run(rules) {
             when: condition_list,
             then: (fact) => then_func(fact, action_dict)
         });
-        console.log(rools_rule);
+        // console.log(rools_rule);
         rools_list.push(rools_rule);
     }
     // console.log(rools_list.length);
-    const rools = new Rools();
     await rools.register(rools_list);
-    await rools.evaluate(facts);
-    console.log(facts);
+    // console.log(rools);
+    // return rools;
+    // await rools.evaluate(facts);
+    // console.log(facts);
 }
 
-run(rules);
+async function evaluate_fact(rule_base, facts) {
+    await rule_base.evaluate(facts);
+}
+
+// Create rule base
+const rools = new Rools();
+create_rule_engine(rules, rools);
+
+// facts
+const facts = {
+    Motion0: "motion",
+    Proximity0: 0.2
+};
+evaluate_fact(rools, facts);
+console.log(facts)
+
+// facts
+const facts_2 = {
+    Motion0: "motion",
+    Proximity0: 0.7
+};
+evaluate_fact(rools, facts_2);
+console.log(facts_2)
+
+
